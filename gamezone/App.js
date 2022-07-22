8042dace1a73f03804200f291130c2837e0638b5
Rodolfo Cabral Neves <roddas360@gmail.com>
2022-07-22 08:27:45 -0300

    Loading the application font on startup

--- gamezone/App.js
+++ gamezone/App.js
@@ -1,6 +1,26 @@
 import Home from "./screens/home";
+import * as Font from 'expo-font';
+import { useState } from "react";
+import {AppLoading} from 'expo';
+
+const getFonts = () => Font.loadAsync({
+    'nunito-regular': require('./assets/font/Nunito-Regular.ttf'),
+    'nunito-bold': require('./assets/font/Nunito-Bold.ttf')
+  });
+
 export default function App() {
-  return (
-    <Home />
-  );
+  
+  const [fontsLoaded,setFontsLoaded] = useState(false);
+
+  if(fontsLoaded)
+  {
+    return (
+      <Home />
+    );
+  }else
+  {
+   return(
+     <AppLoading startAsync={getFonts} onFinish={() => { setFontsLoaded(true) }} />
+   );
+  }
 }
